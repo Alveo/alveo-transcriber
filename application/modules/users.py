2be3ddba2807a8fae79e3011e20a43be32e7cf3b
David L. Irving (SPanda) <david.l.irving@gmail.com>
2017-06-22 14:44:20 +1000

    Initial commit, OAuth2 based

---
+++ application/modules/users.py
@@ -0,0 +1,10 @@
+from application import db, lm
+
+class User(UserMixin, db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    oauth_id = db.Column(db.String(64), nullable=True)
+
+@lm.user_loader
+def load_user(id):
+    return User.query.get(int(id))

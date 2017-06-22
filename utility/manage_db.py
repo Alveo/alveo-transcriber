262451aac9a25886eb9adc3d91c39dd56da8f25d
David L. Irving (SPanda) <david.l.irving@gmail.com>
2017-06-22 11:56:54 +1000

    DB sample/blank generation placeholders

---
+++ utility/manage_db.py
@@ -0,0 +1,15 @@
+from datetime import timedelta
+
+from application import app, db
+
+def gen_sample():
+    db.drop_all()
+    db.create_all()
+
+    # Start adding
+
+def gen_blank():
+    db.drop_all();
+    db.create_all();
+
+    # Start adding

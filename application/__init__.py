e233fcc2c38b8dc58fb26abb356a0a787a15e1d8
David L. Irving (SPanda) <david.l.irving@gmail.com>
2017-06-22 15:52:14 +1000

    Added RAuth/OAuth2 setup

--- application/__init__.py
+++ application/__init__.py
@@ -5,6 +5,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_mail import Mail
 from flask_login import LoginManager, UserMixin
+from rauth import OAuth2Service
 
 app = Flask(__name__)
 BASE_FOLDER = os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..'))
@@ -18,5 +19,14 @@ migrate = Migrate(app, db)
 
 app.config['SQLALC_INSTANCE'] = db
 app.config['MAILADDR'] = mail
+
+oauth_alveo = OAuth2Service(
+    name='alveo',
+    client_id=app.config['OAUTH_ALVEO_APPID'],
+    client_secret=app.config['OAUTH_ALVEO_APPSEC'],
+    authorize_url='https://example.com/oauth/authorize',           
+    access_token_url='https://example.com/oauth/access_token',
+    base_url='https://example.com/'
+)
 
 import application.views

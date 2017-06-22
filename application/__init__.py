7a5c732f6d136ea17bfbede14258b4ad1fe8e2a9
David L. Irving (SPanda) <david.l.irving@gmail.com>
2017-06-22 12:45:47 +1000

    Added mail imports/config

--- application/__init__.py
+++ application/__init__.py
@@ -3,16 +3,18 @@ import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_mail import Mail
 
-#static_url_path for remote if ever needed
 app = Flask(__name__)
 BASE_FOLDER = os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..'))
 app.static_folder = os.path.join(BASE_FOLDER, 'static')
 app.config.from_pyfile('../config')
 
+mail = Mail(app)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
 app.config['SQLALC_INSTANCE'] = db
+app.config['MAILADDR'] = mail
 
-#import application.views
+import application.views

5f6b186fc8c088c8a42f0c455cbffd04f6a7b503
David L. Irving (SPanda) <david.l.irving@gmail.com>
2017-06-22 16:14:34 +1000

    Fixed missing import, Added logout view

--- application/modules/users.py
+++ application/modules/users.py
@@ -1,3 +1,8 @@
+from flask import redirect, url_for
+from flask_login import UserMixin
+from flask.views import MethodView
+from flask_login import current_user
+
 from application import db, lm
 
 class User(UserMixin, db.Model):
@@ -8,3 +13,12 @@ class User(UserMixin, db.Model):
 @lm.user_loader
 def load_user(id):
     return User.query.get(int(id))
+
+class LogoutView(MethodView):
+    def get(self):
+        if not current_user.is_anonymous:
+            logout_user(user)
+
+        return redirect(url_for('index_view'))
+
+logout_view = LogoutView.as_view('logout_view')

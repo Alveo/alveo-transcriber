5b933848be17dbee16eb10871544e8b4c4b58bcd
David L. Irving (SPanda) <david.l.irving@gmail.com>
2017-06-22 12:39:51 +1000

    Added 403, 404 handlers. Added 500 handler with SMTP support.

---
+++ application/modules/error.py
@@ -0,0 +1,39 @@
+import traceback
+from threading import Thread
+
+from flask import render_template
+from flask_mail import Message
+
+# Flask error for 403
+def not_allowed(error):
+    context = {
+            }
+    return render_template('403.html', **context), 403
+
+# Flask error for 404
+def not_found(error):
+    context = {
+            }
+    return render_template('404.html', **context), 404
+
+
+def async(f):
+    def wrapper(*args, **kwargs):
+        thr = Thread(target = f, args = args, kwargs = kwargs)
+        thr.start()
+    return wrapper
+
+@async
+def send_async_email(message):
+    with app.app_context():
+        app.config['MAILADDR'].send(message)
+
+# Flask error for 500
+def server_error(error):
+    message = Message(recipients=app.config['ADMINS'], subject=app.config['ERROR_HEADER_500'], body=traceback.format_exc())
+    send_async_email(message)
+
+    context = {
+            }
+
+    return render_template('500.html', **context), 500

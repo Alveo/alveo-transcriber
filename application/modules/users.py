8f2ce27f456afcbf0a9cdb2e15a6264500d8c324
David L. Irving (SPanda) <david.l.irving@gmail.com>
2017-06-22 15:18:54 +1000

    oauth_id no longer nullable

--- application/modules/users.py
+++ application/modules/users.py
@@ -3,7 +3,7 @@ from application import db, lm
 class User(UserMixin, db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
-    oauth_id = db.Column(db.String(64), nullable=True)
+    oauth_id = db.Column(db.String(64), nullable=False)
 
 @lm.user_loader
 def load_user(id):

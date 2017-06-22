95602d36180c48c97b3058f41edd930ae7dde47e
David L. Irving (SPanda) <david.l.irving@gmail.com>
2017-06-22 12:52:57 +1000

    Added error handler config, init placeholder

---
+++ application/views.py
@@ -0,0 +1,12 @@
+from flask import url_for
+
+from application import app
+from application.modules.error import not_allowed,  not_found, server_error
+
+app.register_error_handler(403, not_allowed)
+app.register_error_handler(404, not_found)
+app.register_error_handler(500, server_error)
+
+@app.before_first_request
+def init():
+    pass

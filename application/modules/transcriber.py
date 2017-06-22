62f5d2a464dddd3d2fdb336fb56f687ef373e633
David L. Irving (SPanda) <david.l.irving@gmail.com>
2017-06-22 16:26:46 +1000

    Added logout functionality

--- application/modules/transcriber.py
+++ application/modules/transcriber.py
@@ -1,12 +1,14 @@
 from flask import render_template
 from flask.views import MethodView
+from flask_login import current_user
 
 from application import db
 
 class IndexView(MethodView):
     def get(self):
         context = {
-                }
+                "logged_in": not current_user.is_anonymous,
+            }
 
         return render_template('index.html', **context)
 

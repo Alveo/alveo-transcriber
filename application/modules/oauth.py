b74eccaf0320570f1801b158d6a422a576e72d04
David L. Irving (SPanda) <david.l.irving@gmail.com>
2017-06-22 16:08:03 +1000

    Implemented basic OAuth (does not support Alveo yet)

---
+++ application/modules/oauth.py
@@ -0,0 +1,58 @@
+from flask import redirect, url_for, session, request, flash
+from flask.views import MethodView
+from flask_login import current_user
+
+from application import app, oauth_alveo
+
+class AlveoAuthoriseView(MethodView):
+    def get(self):
+        if not current_user.is_anonymous:
+            return redirect(url_for('index_view'))
+
+        return redirect(oauth_alveo.get_authorize_url(
+            scope='email',
+            response_type='code',
+            redirect_uri=url_for('alveo_callback_view', _external=True)
+            )
+        )
+
+class AlveoCallbackView(MethodView):
+    def get(self):
+        if not current_user.is_anonymous:
+            return redirect(url_for('index_view'))
+
+        def callback():
+            request_token = session.pop('request_token', None)
+
+            if 'oauth_verifier' not in request.args:
+                return None
+
+            oauth_session = oauth_alveo.get_auth_session(
+                request_token[0],
+                request_token[1],
+                data={'oauth_verifier': request.args['oauth_verifier']}
+            )
+
+            data = oauth_session.get('account/verify_credentials.json').json()
+            oauth_id  = str(data.get('oauth_id'))
+
+            return oauth_id
+
+        oauth_id = callback() # supports multiple arguments, e.g oauth_id, firstname
+
+        if oauth_id is None: # No valid oauth_id
+            flash('Authentication failed.')
+            return redirect(url_for('index_view'))
+
+        user = User.query.filter_by(oauth_id=oauth_id).first()
+        if not user: # Create one
+            user = User(oauth_id=oauth_id)
+            db.session.add(user)
+            db.session.commit()
+
+        login_user(user, True)
+
+        return redirect(url_for('index'))
+
+alveo_authorise_view = AlveoAuthoriseView.as_view('alveo_authorise_view')
+alveo_callback_view = AlveoCallbackView.as_view('alveo_callback_view')

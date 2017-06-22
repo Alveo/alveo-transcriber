7abc8f60b9c893ee9520e08f136475662d44048d
David L. Irving (SPanda) <david.l.irving@gmail.com>
2017-06-22 15:33:34 +1000

    Added OAuth routes

--- application/views.py
+++ application/views.py
@@ -2,6 +2,7 @@ from flask import url_for
 
 from application import app
 from application.modules.transcriber import index_view
+from application.modules.oauth import alveo_authorise_view, alveo_callback_view
 from application.modules.error import not_allowed,  not_found, server_error
 
 app.register_error_handler(403, not_allowed)
@@ -9,6 +10,8 @@ app.register_error_handler(404, not_found)
 app.register_error_handler(500, server_error)
 
 app.add_url_rule('/', view_func=index_view, methods=['GET',])
+app.add_url_rule('/oauth/authorise', view_func=alveo_authorise_view, methods=['GET',])
+app.add_url_rule('/oauth/callback', view_func=alveo_callback_view, methods=['GET',])
 
 @app.before_first_request
 def init():

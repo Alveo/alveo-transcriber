e2d83b20360aa32a8a11d4ab328e8f748492cf43
David L. Irving (SPanda) <david.l.irving@gmail.com>
2017-06-22 12:54:19 +1000

    Added index rule

--- application/views.py
+++ application/views.py
@@ -7,6 +7,8 @@ app.register_error_handler(403, not_allowed)
 app.register_error_handler(404, not_found)
 app.register_error_handler(500, server_error)
 
+app.add_url_rule('/', view_func=index, methods=['GET',])
+
 @app.before_first_request
 def init():
     pass

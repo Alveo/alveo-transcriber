c8135d80c6807e655593f3a2550ee5b5e5319bcc
David L. Irving (SPanda) <david.l.irving@gmail.com>
2017-06-22 12:58:09 +1000

    Added missing import

--- application/views.py
+++ application/views.py
@@ -1,13 +1,14 @@
 from flask import url_for
 
 from application import app
+from application.modules.transcriber import index_view
 from application.modules.error import not_allowed,  not_found, server_error
 
 app.register_error_handler(403, not_allowed)
 app.register_error_handler(404, not_found)
 app.register_error_handler(500, server_error)
 
-app.add_url_rule('/', view_func=index, methods=['GET',])
+app.add_url_rule('/', view_func=index_view, methods=['GET',])
 
 @app.before_first_request
 def init():

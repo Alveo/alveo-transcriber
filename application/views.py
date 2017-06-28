9e5812150e2be2e7f1493ff19822b0ef7f4a76a2
David L. Irving (SPanda) <david.l.irving@gmail.com>
2017-06-28 18:47:07 +1000

    Added REST pull for clips

--- application/views.py
+++ application/views.py
@@ -1,3 +1,4 @@
+import json
 from flask import redirect
 
 from application import app
@@ -20,3 +21,85 @@ def init():
 @app.route('/')
 def serve():
     return redirect('/static/index.html')
+
+@app.route('/request-data')
+def request():
+    # Confirm user is authenticated
+    #  Confirm user can receive more
+    #   Pull data out
+    return json.dumps({'clips':[{
+            "id": "0",
+            "label": "TestWWWWWWWWWW",
+            "audio_url": "",
+            "duration": 4730,
+            "segments": [
+                {
+                    "start": 0.000,
+                    "end": 7.1239,
+                    "speaker": "1",
+                    "annotation": "",
+                },
+                {
+                    "start": 8.2383,
+                    "end": 14.2395,
+                    "speaker": 2,
+                    "annotation": "",
+                },
+                {
+                    "start": 16.2333,
+                    "end": 19.2395,
+                    "speaker": "",
+                    "annotation": "",
+                }
+            ]
+        },
+        {
+            "id": "1",
+            "label": "Test Test Test Test Test Test Test Test Test Test Test Test",
+            "audio_url": "",
+            "duration": "63812",
+            "segments": [
+                {
+                    "start": 0.000,
+                    "end": 5.732,
+                    "speaker": "1",
+                    "annotation": "",
+                },
+                {
+                    "start": 6.9271,
+                    "end": 11.3293,
+                    "speaker": "",
+                    "annotation": "",
+                }]
+        },
+        {
+            "id": "2",
+            "label": "Audio.wav",
+            "audio_url": "",
+            "duration": "4232000",
+            "segments": [
+                {
+                    "start": 0.000,
+                    "end": 1.763,
+                    "speaker": "1",
+                    "annotation": "",
+                },
+                {
+                    "start": 2.132,
+                    "end": 6.423,
+                    "speaker": "",
+                    "annotation": "",
+                },
+                {
+                    "start": 7.432,
+                    "end": 11.1292,
+                    "speaker": "1",
+                    "annotation": "Testing",
+                },
+                {
+                    "start": 14.3923,
+                    "end": 23.2391,
+                    "speaker": "2",
+                    "annotation": "",
+                }]
+        }]})

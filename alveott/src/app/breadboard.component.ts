1bc4c0a39644c751feb2452e7b2d01cf5a137760
David L. Irving (SPanda) <david.l.irving@gmail.com>
2017-07-12 17:02:04 +1000

    Updated breadboard to provide a click option for data retrieval source

--- alveott/src/app/breadboard.component.ts
+++ alveott/src/app/breadboard.component.ts
@@ -1,23 +1,35 @@
 import { Component } from '@angular/core';
-import { SessionService } from './session.service';
 import { DataService } from './data.service';
-import { OnInit } from '@angular/core';
 
 import { Clip } from './clip';
 
 @Component({
   selector: 'breadboard',
   template: `
+  <div style="text-align: center">
+    <button (click)="this.click_remote()">Pull from remote</button>
+    <button (click)="this.click_local()">Pull from local</button>
+  </div>
   `,
 })
 
-export class BreadboardComponent implements OnInit {
+export class BreadboardComponent {
+  constructor(
+    public dataService: DataService,
+  ) {}
 
-  constructor(public sessionService: SessionService, 
-              public dataService: DataService) { }
-  ngOnInit(): void {
+  click_remote(): void {
     this.dataService.getData();
     this.dataService.getFile();
+    this.dataService.startStore();
   }
 
+  click_local(): void {
+    this.dataService.database.get("clips").then(results => {
+      this.dataService.clips = results.clips;
+      console.log(results.clips);
+    });
+    this.dataService.getFile();
+    this.dataService.startStore();
+  }
 }

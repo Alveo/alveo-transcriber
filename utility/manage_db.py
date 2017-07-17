6adfe06da335c5888c1e3cef51606113ebd28551
David L. Irving (SPanda) <david.l.irving@gmail.com>
2017-07-17 12:58:45 +1000

    Added test user fixture

--- utility/manage_db.py
+++ utility/manage_db.py
@@ -1,15 +1,19 @@
 from datetime import timedelta
 
 from pyapplication import app, db
+from pyapplication.modules.user_model import User
 
 def gen_sample():
     db.drop_all()
     db.create_all()
 
-    # Start adding
+    testUser = User(alias='test')
+    db.session.add(testUser)
+
+    db.session.commit();
 
 def gen_blank():
     db.drop_all();
     db.create_all();
 
-    # Start adding
+    db.session.commit();

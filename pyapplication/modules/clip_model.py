952e0fc57e8dce6ea320646d6344c9ab6b6ca3a8
David L. Irving (SPanda) <david.l.irving@gmail.com>
2017-07-17 13:58:14 +1000

    Added constructor/repr handler

--- pyapplication/modules/clip_model.py
+++ pyapplication/modules/clip_model.py
@@ -5,3 +5,10 @@ class Clip(db.Model):
 
     audio_url = db.Column(db.String(256), nullable=False)
     sample_data = db.Column(db.String(4096), nullable=False)
+
+    def __init__(self, audio_url=None, sample_data=None):
+        self.audio_url = audio_url
+        self.sample_data = sample_data
+
+    def __repr__(self):
+        return self.audio_url;

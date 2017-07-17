798a07b9ecbcf207597a5de731526eabb1208af2
David L. Irving (SPanda) <david.l.irving@gmail.com>
2017-07-17 12:58:17 +1000

    Added constructor, allow oauth id to be null

--- pyapplication/modules/user_model.py
+++ pyapplication/modules/user_model.py
@@ -4,7 +4,15 @@ from pyapplication import db, lm
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    oauth_id = db.Column(db.String(64), nullable=False)
+    oauth_id = db.Column(db.String(64), nullable=True) # Allow non-OAuth users
+    alias = db.Column(db.String(64), nullable=False)
+
+    def __init__(self, oauth_id=None, alias=None):
+        self.alias = alias
+        self.oauth_id = oauth_id
+
+    def __repr__(self):
+        return self.alias;
 
 @lm.user_loader
 def load_user(id):

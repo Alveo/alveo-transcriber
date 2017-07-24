f80ceceb0cef95221282525fa5d1905c1b220657
David L. Irving (SPanda) <david.l.irving@gmail.com>
2017-07-24 17:09:55 +1000

    Added Alveo service integration

--- src/app/itemlists.component.ts
+++ src/app/itemlists.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
 import { AppUtilService } from './app-util.service';
@@ -9,12 +9,23 @@ import { AppUtilService } from './app-util.service';
   styleUrls: ['./itemlists.component.css'],
 })
 
-export class ItemListsComponent {
+export class ItemListsComponent implements OnInit {
   constructor(
     public router: Router,
     public appService: AppUtilService) { }
 
-  onSelect(clip): void {
+  ngOnInit() {
+    this.appService.alveo.pullToken(this.appService.auth.key);
+    setTimeout(() => {this.appService.alveo.pullAPI()}, 3000);
+    setTimeout(() => {this.appService.alveo.pullLists()}, 5000);
+  }
+
+  getLists(): any {
+    console.log(this.appService.alveo.lists);
+    return this.appService.alveo.lists;
+  }
+
+  onSelect(list): void {
     //this.router.navigate(['./itemlist'], { queryParams: { item: item }});
   }
 }

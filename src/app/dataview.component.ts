d0b8948aebd16fc0fd2f9a400a5780a2813782d3
David L. Irving (SPanda) <david.l.irving@gmail.com>
2017-07-28 17:43:48 +1000

    Implementation/migration to DataViewComponent

--- src/app/dataview.component.ts
+++ src/app/dataview.component.ts
@@ -16,7 +16,44 @@ export class DataViewComponent {
     public router: Router,
     public appService: AppUtilService) { }
 
-  onSelect(list): void {
-    //this.router.navigate(['./items']);
+  private getList(): any {
+    return this.appService.alveo.getActiveList();
+  }
+
+  private getData(): any {
+    return this.appService.alveo.getActiveListData(); // If it doesn't exist, it will be pulled
+  }
+
+  getListName(): any {
+    let list = this.getList();
+    if (list == null) {
+      return "(Loading)"
+    }
+    return list.name;
+  }
+
+  getListSize(): any {
+    let list = this.getList();
+    if (list == null) {
+      return "(...)"
+    }
+    return list.num_items;
+  }
+
+  getItems(): any {
+    let list = this.getList();
+    if (list == null) {
+      return []
+    }
+    
+    let items = [];
+    for (let item of this.getData()) {
+      items.push(item);
+    }
+    return items;
+  }
+
+  actionBack(): void {
+    this.router.navigate(['./itemlists']);
   }
 }
--- src/app/items.component.ts
+++ src/app/items.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { Router } from '@angular/router';
 
 import { AppUtilService } from './app-util.service';
@@ -10,53 +10,14 @@ import { AppUtilService } from './app-util.service';
 })
 
 export class ItemsComponent {
+  @Input() items: any;
+  @Input() selection: any;
+
   constructor(
     public router: Router,
     public appService: AppUtilService) { }
 
-  private getList(): any {
-    return this.appService.alveo.getActiveList();
-  }
-
-  private getData(): any {
-    return this.appService.alveo.getActiveListData(); // If it doesn't exist, it will be pulled
-  }
-
-  getListName(): any {
-    let list = this.getList();
-    if (list == null) {
-      return "(Loading)"
-    }
-    return list.name;
-  }
-
-  getListSize(): any {
-    let list = this.getList();
-    if (list == null) {
-      return "(...)"
-    }
-    return list.num_items;
-  }
-
-  getItems(): any {
-    let list = this.getList();
-    if (list == null) {
-      return []
-    }
-    
-    let items = [];
-    for (let item of this.getData()) {
-      items.push(item);
-    }
-    return items;
-  }
-
-  onSelect(list): void {
-    //this.appSerice.alveo.pullList(list.item_list_url);
-    //this.router.navigate(['./itemlist']);
-  }
-
-  actionBack(): void {
-    this.router.navigate(['./itemlists']);
+  onSelect(item): void {
+    this.selection = item;
   }
 }

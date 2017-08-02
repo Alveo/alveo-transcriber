25d8760bf75d9d9b7b1bff38121d2f00110022fd
David L. Irving (SPanda) <david.l.irving@gmail.com>
2017-08-02 13:33:25 +1000

    Added internal/external bindings

--- src/app/oauth2.service.ts
+++ src/app/oauth2.service.ts
@@ -11,8 +11,12 @@ export class OAuth2Service {
   baseURL: string = "https://staging.alveo.edu.au";
   loginURL: string = this.baseURL+"/oauth/authorize";
 
-  clientID: string = "b1692ca827a959f62a9b79e0eb471c9fdc3e818c33c976076f7948101ba23084";
-  clientSecret: string = "e533af5728a1334a089d9b446bda3204be4d59785734981832956b446cfbf64b";
+  // Internal Binding
+  clientID: string = "5f891b901fd60f3c0dd3b81a2376491b6cb092b4de77bdc2d7c49362e8d3d02b";
+  clientSecret: string = "7584a0029874541e7bef236cd6f1634e6c6c0131b5537f57145bfe6473409d40";
+  // External Binding
+  //clientID: string = "b1692ca827a959f62a9b79e0eb471c9fdc3e818c33c976076f7948101ba23084";
+  //clientSecret: string = "e533af5728a1334a089d9b446bda3204be4d59785734981832956b446cfbf64b";
   callbackURL: string = window.location.origin+ "/oauth/callback";
   authCode: string;
   token: string;

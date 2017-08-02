1766c31db718ffe27738ff662d66770216a52512
David L. Irving (SPanda) <david.l.irving@gmail.com>
2017-08-02 17:58:22 +1000

    Removed deprecated types

--- src/app/clip.ts
+++
@@ -1,9 +0,0 @@
-import { Segment } from './segment';
-
-export class Clip {
-  id: number;
-  label: string;
-  audio_url: string;
-  duration: number;
-  segments: Segment[];
-}
--- src/app/segment.ts
+++
@@ -1,6 +0,0 @@
-export class Segment {
-  start: number;
-  end: number;
-  speaker: string;
-  annotation: string;
-}

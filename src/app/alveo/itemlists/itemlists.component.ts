4326b9e9c7d5b1965bc814975b3cf8452c7d357d
David L. Irving (SPanda) <david.l.irving@gmail.com>
2017-09-14 18:09:29 +1000

    Added list table integration

--- src/app/alveo/itemlists/itemlists.component.ts
+++ src/app/alveo/itemlists/itemlists.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, } from '@angular/core';
+
 import { Router } from '@angular/router';
 
 import { AuthService } from '../shared/auth.service';

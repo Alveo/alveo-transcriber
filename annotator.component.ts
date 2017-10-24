b3b3c1f001eed1a3f4ee716a40e3234ae0b276f1
David L. Irving (SPanda) <david.l.irving@gmail.com>
2017-10-25 01:34:00 +1100

    Support CSV/JSON exportation

--- annotator.component.ts
+++ annotator.component.ts
@@ -21,6 +21,35 @@ export class AnnotatorComponent {
     this.router.navigate(['/']);
   }
 
+  downloadFile(url, filename): void {
+    let anchor = document.createElement("a");
+    anchor.download = filename;
+    console.log(anchor.download);
+    anchor.href = url;
+    window.document.body.appendChild(anchor);
+    anchor.click();
+    window.document.body.removeChild(anchor);
+    URL.revokeObjectURL(url);
+  }
+
+  exportCSV(): void {
+    let csv = this.annotatorService.dumpCSV();
+
+    let blob = new Blob([csv], { type: 'text/csv' });
+    let url = window.URL.createObjectURL(blob);
+
+    this.downloadFile(url, this.annotatorService.getAudioFileName() + ".csv");
+  }
+
+  exportJSON(): void {
+    let csv = this.annotatorService.dumpJSON();
+
+    let blob = new Blob([csv], { type: 'application/json' });
+    let url = window.URL.createObjectURL(blob);
+
+    this.downloadFile(url, this.annotatorService.getAudioFileName() + ".json");
+  }
+
   actionSegment(): void {
     this.segService.segment(this.getAudioFileURL(),
       (data) => {

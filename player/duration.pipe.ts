8f271fcf830ca1824ca44b55e4194e84c0065d3a
David L. Irving (SPanda) <david.l.irving@gmail.com>
2017-10-24 19:49:36 +1100

    Implement annotations to some extent
    Major bug(?) with ngModel currently

--- player/duration.pipe.ts
+++ player/duration.pipe.ts
@@ -1,7 +1,7 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({ name: 'duration' })
-export class DurationPipe implements PipeTransform{
+export class DurationPipe implements PipeTransform {
   transform(input: number): string {
     var seconds = Math.floor((input % 60000) / 1000);
     var minutes = Math.floor(input / 60000);
@@ -14,7 +14,7 @@ export class DurationPipe implements PipeTransform{
 }
 
 @Pipe({ name: 'durationshort' })
-export class DurationShortPipe implements PipeTransform{
+export class DurationShortPipe implements PipeTransform {
   transform(input: number): string {
     var seconds = Math.floor((input % 60));
     var minutes = Math.floor(input / 60);
--- shared/annotator.service.ts
+++ shared/annotator.service.ts
@@ -1,12 +1,33 @@
 import { Injectable } from '@angular/core';
 
+export class Annotation {
+  start: number;
+  end: number;
+  speaker: string;
+  annotation: string;
+
+  constructor(start: number, end: number,
+              speaker: string, annotation: string) {
+    this.start = start;
+    this.end = end;
+    this.speaker = speaker;
+    this.annotation = annotation;
+  }
+}
+
 @Injectable()
 export class AnnotatorService {
   audioFile: any;
-  annotations: Array<any>;
+  audioFileName: string;
+  annotations: any;
 
   getAnnotations(): any {
-    return this.annotations;
+    //return this.annotations;
+    let a = [
+      new Annotation(0.00, 3.20, "Test", "Test"),
+      new Annotation(4.00, 8.20, "Test", "Test")
+    ];
+    return a;
   }
 
   getAudioFile(): any {
@@ -14,6 +35,6 @@ export class AnnotatorService {
   }
 
   getAudioFileName(): any {
-    return "null";
+    return this.audioFileName;
   }
 }

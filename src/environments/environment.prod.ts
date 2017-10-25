b536f75466084b862d210d32e8a72bfd507bdbd4
David L. Irving (SPanda) <david.l.irving@gmail.com>
2017-10-25 14:03:27 +1100

    Temporarily set production to staging server

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,10 @@
 export const environment = {
   production: true,
 
-  baseURL: "https://www.alveo.edu.au",
-  loginURL: "http://www.alveo.edu.au/oauth/authorize",
+  //baseURL: "https://www.alveo.edu.au",
+  //loginURL: "http://www.alveo.edu.au/oauth/authorize",
+  baseURL: "https://staging.alveo.edu.au",
+  loginURL: "https://staging.alveo.edu.au/oauth/authorize",
 
   clientID: "b1692ca827a959f62a9b79e0eb471c9fdc3e818c33c976076f7948101ba23084",
   clientSecret: "e533af5728a1334a089d9b446bda3204be4d59785734981832956b446cfbf64b",

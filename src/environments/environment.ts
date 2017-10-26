2885f68f666a2f5fcbf71f0e2e3a9b9ccb3beba0
David L. Irving (SPanda) <david.l.irving@gmail.com>
2017-10-27 00:00:02 +1100

    Support staging environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -9,11 +9,6 @@ export const environment = {
   baseURL: "https://staging.alveo.edu.au",
   loginURL: "https://staging.alveo.edu.au/oauth/authorize",
 
-  // Internal Binding
-  //clientID: "5f891b901fd60f3c0dd3b81a2376491b6cb092b4de77bdc2d7c49362e8d3d02b";
-  //clientSecret: "7584a0029874541e7bef236cd6f1634e6c6c0131b5537f57145bfe6473409d40";
-  
-  // External Binding
   clientID: "2a487cc09672bdb26ddb746512ecdbbbad7934793f0de53402afb7ca9ec07f9b",
   clientSecret: "4886aedfa49294393749b2db813c18f7096ee6eaa6e29f40e990e4788123988e",
 

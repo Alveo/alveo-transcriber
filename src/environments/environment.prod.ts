fa37170ef06e94ccc86521b2b711051f57a02c9c
David L. Irving (SPanda) <david.l.irving@gmail.com>
2018-01-09 17:11:19 +1100

    Update URLs

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
 
-  baseURL: 'https://www.alveo.edu.au',
-  loginURL: 'https://www.alveo.edu.au/oauth/authorize',
+  baseURL: 'https://app.alveo.edu.au',
+  loginURL: '/oauth/authorize',
 
   clientID: 'b1692ca827a959f62a9b79e0eb471c9fdc3e818c33c976076f7948101ba23084',
   clientSecret: 'e533af5728a1334a089d9b446bda3204be4d59785734981832956b446cfbf64b',
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: false,
 
-  baseURL: 'https://staging.alveo.edu.au',
-  loginURL: 'https://staging.alveo.edu.au/oauth/authorize',
+  baseURL: 'https://app.alveo.edu.au',
+  loginURL: '/oauth/authorize',
 
   clientID: '2a487cc09672bdb26ddb746512ecdbbbad7934793f0de53402afb7ca9ec07f9b',
   clientSecret: '4886aedfa49294393749b2db813c18f7096ee6eaa6e29f40e990e4788123988e',

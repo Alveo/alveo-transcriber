6c2e6351da99510175cf1690d863d3df47f3ae7c
David L. Irving (SPanda) <david.l.irving@gmail.com>
2018-01-17 18:57:40 +1100

    Support use of PouchDB to save and store sessions automatically

--- src/app/alveo/shared/session.service.ts
+++ src/app/alveo/shared/session.service.ts
@@ -6,14 +6,44 @@ import { DBService } from './db.service';
 
 @Injectable()
 export class SessionService {
-  private active_route: any[];
-  private selected_list: any = null;
-  private selected_doc: any = null;
-  private selected_doc_data: ArrayBuffer = null;
+  private active_route: any = [];
+  private active_list: any = null;
+  private active_doc: any = null;
+  private active_doc_data: ArrayBuffer = null;
+  private load_init = true;
 
   constructor(
     private router: Router,
     private dbService: DBService) {
+    this.dbService.get("sessionService").then(
+      data => {
+        console.log("Stored session data has been found and loaded.");
+
+        this.active_list = data['active_list'];
+        this.active_doc = data['active_doc'];
+        this.active_doc_data = data['active_doc_data'];
+
+        this.load_init = false;
+
+        this.navigate(data['active_route']).subscribe();
+      },
+      error => {
+        console.log("Stored session data not found. Initialising.");
+
+        this.updateStorage();
+
+        this.load_init = false;
+      }
+    );
+  }
+
+  private updateStorage() {
+    this.dbService.put("sessionService", {
+      "active_route": this.active_route,
+      "active_list": this.active_list,
+      "active_doc": this.active_doc,
+      "active_doc_data": this.active_doc_data,
+    });
   }
 
   private dbRequest(storageName: string): Observable<any> {
@@ -38,6 +68,7 @@ export class SessionService {
       this.router.navigate(route)
         .then(data => {
           this.active_route = route;
+          this.updateStorage();
           observer.next(data);
           observer.complete();
         })
@@ -48,29 +79,34 @@ export class SessionService {
   }
 
   public resetSession() {
-    this.setActiveList(null);
-    this.setActiveDoc(null, null);
     this.navigate(['/']).subscribe();
+
+    if (!this.load_init) {
+      this.setActiveList(null);
+      this.setActiveDoc(null, null);
+    }
   }
 
   public setActiveList(list: any) {
-    this.selected_list = list;
+    this.active_list = list;
+    this.updateStorage();
   }
 
   public getActiveList(): any {
-    return this.selected_list;
+    return this.active_list;
   }
 
   public setActiveDoc(doc: any, docData: ArrayBuffer) {
-    this.selected_doc = doc;
-    this.selected_doc_data = docData;
+    this.active_doc = doc;
+    this.active_doc_data = docData;
+    this.updateStorage();
   }
 
   public getActiveDoc(): any {
-    return this.selected_doc;
+    return this.active_doc;
   }
 
   public getActiveDocData(): any {
-    return this.selected_doc_data;
+    return this.active_doc_data;
   }
 }

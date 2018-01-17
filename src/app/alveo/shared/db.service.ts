0b621e1136bdf3945e6d12ac2b97a016a9bf9a0d
David L. Irving (SPanda) <david.l.irving@gmail.com>
2018-01-17 18:57:16 +1100

    Support 409s, destroy DB

--- src/app/alveo/shared/db.service.ts
+++ src/app/alveo/shared/db.service.ts
@@ -10,6 +10,17 @@ export class DBService {
     this.database = new PouchDB('alveott');
   }
 
+  public clear() {
+    this.database.destroy().then(
+      data => {
+        this.database = new PouchDB('alveott');
+      },
+      error => {
+        console.log(error);
+      }
+    );
+  }
+
   public get(key: string) {
     return this.database.get(key);
   }
@@ -17,12 +28,16 @@ export class DBService {
   public put(key: string, value: any) {
     value._id = key;
     return this.get(key).then(result => {
-        value._rev = result._rev;
-        return this.database.put(value);
-    }, error => {
+      value._rev = result._rev;
+      return this.database.put(value);
+    }).catch(error => {
       if (error.status === 404) {
-          return this.database.put(value);
-      } else {
+        return this.database.put(value);
+      }
+      else if (error.status === 409) {
+        return this.put(key, value);
+      }
+      else {
         console.log(error);
       }
     });

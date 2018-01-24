92a775911198767717eb7c543e20b85ae2fd3974
David L. Irving (SPanda) <david.l.irving@gmail.com>
2018-01-25 02:35:18 +1100

    Update build envs

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,6 @@ export const environment = {
   baseURL: 'https://app.alveo.edu.au',
   loginURL: '/oauth/authorize',
 
-  //clientID: '2a487cc09672bdb26ddb746512ecdbbbad7934793f0de53402afb7ca9ec07f9b',
-  //clientSecret: '4886aedfa49294393749b2db813c18f7096ee6eaa6e29f40e990e4788123988e',
   clientID: 'e37df76b698449298761b34c14654ba871e694f81be42cac483c1dc8b529279e',
   clientSecret: '2b83c1b2382d0f76b62729af99a4dba64aac61dcf7827a4d5df60e98c1975055',
 

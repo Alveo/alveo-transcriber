c8368d31cc76b06122e54e325ce07af1271ff1aa
David L. Irving (SPanda) <david.l.irving@gmail.com>
2018-01-24 23:19:20 +1100

    Support new annotator events

--- src/app/alveo/shared/annotations.service.ts
+++ src/app/alveo/shared/annotations.service.ts
@@ -1,18 +1,28 @@
-import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs/Observable';
-import 'rxjs/Observable';
+import { Injectable, EventEmitter } from '@angular/core';
 
 import { AnnotatorService } from '../../annotator/shared/annotator.service';
 import { Annotation } from '../../annotator/shared/annotator.service';
+import { SegmentorService } from './segmentor.service';
 import { DBService, Databases } from './db.service';
 
 @Injectable()
 export class AnnotationsService {
-  private annotatorWatcher: any = null;
+  private annotationsEvent: any = null;
+  private annotatorEvent: any = null;
+
+  private docIdentifier: string = '';
+  private fileUrl: string = '';
+
+  public serviceEvent: EventEmitter<any> = new EventEmitter();
 
   constructor(
     private annotatorService: AnnotatorService,
+    private segmentorService: SegmentorService,
     private dbService: DBService) {}
+
+  public setFileUrl(url: string) {
+    this.fileUrl = url;
+  }
 
   public getAnnotations(identifier: string): Promise<any> {
     return new Promise(
@@ -24,22 +34,51 @@ export class AnnotationsService {
     );
   }
 
+  private autoSegment(fileUrl: string) {
+    if (fileUrl === "") {
+      //raise exc
+    }
+
+    this.segmentorService.segment(fileUrl,
+      (data) => {
+        this.annotatorService.rebuild(data.json());
+        this.setAnnotations(this.docIdentifier, data.json());
+      }
+    );
+  }
+
   public setAnnotations(docIdentifier: string, annotations: Array<Annotation>): Promise<any> {
     return this.dbService.instance(Databases.Annotations)
       .put(docIdentifier, {"annotations":annotations});
   }
 
-  public watch(docIdentifier: string, watcher: any) {
-    if (this.annotatorWatcher != null) {
-      this.annotatorWatcher.unsubscribe();
-      this.annotatorWatcher = null;
+  public watch(docIdentifier: string, annotatorWatcher: any, annotationWatcher: any) {
+    if (this.annotatorEvent != null) {
+      this.annotatorEvent.unsubscribe();
+      this.annotatorEvent = null;
     }
-    this.annotatorWatcher = watcher.subscribe((event:any) => {
+
+    if (this.annotationsEvent != null) {
+      this.annotationsEvent.unsubscribe();
+      this.annotationsEvent = null;
+    }
+
+    this.docIdentifier = docIdentifier;
+
+    this.annotatorEvent = annotatorWatcher.subscribe((event:any) => {
+      if (event === "exit") {
+        this.serviceEvent.emit("exit");
+      } else if (event === "autosegment") {
+        this.autoSegment(this.fileUrl);
+      }
+    });
+
+    this.annotationsEvent = annotationWatcher.subscribe((event:any) => {
       this.setAnnotations(docIdentifier, this.annotatorService.annotations);
     });
   }
 
-  public prepareAnnotator(identifier: string, audioFile: ArrayBuffer, backUrl: Array<any>): Promise<any> {
+  public prepareAnnotator(identifier: string, audioFile: ArrayBuffer): Promise<any> {
     return new Promise(
       (resolve, reject) => {
         this.getAnnotations(identifier).then(
@@ -47,10 +86,12 @@ export class AnnotationsService {
             this.annotatorService.rebase(data);
 
             this.annotatorService.audioFile = audioFile;
-            this.annotatorService.setBackUrl(backUrl);
 
             this.annotatorService.audioFileName = identifier;
-            this.watch(identifier, this.annotatorService.annotationsUpdate);
+            this.watch(identifier,
+              this.annotatorService.externalEvent,
+              this.annotatorService.annotationsUpdate
+            );
             resolve();
           },
           (error) => {

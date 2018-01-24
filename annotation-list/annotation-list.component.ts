5526053d04b62defc44e0856721c11817384f430
David L. Irving (SPanda) <david.l.irving@gmail.com>
2018-01-25 02:17:14 +1100

    Support region selection and visuals

--- annotation-list/annotation-list.component.ts
+++ annotation-list/annotation-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnChanges } from '@angular/core';
+import { Component, Input, OnChanges, OnInit } from '@angular/core';
 
 import { Annotation } from '../shared/annotator.service';
 import { AnnotatorService } from '../shared/annotator.service';
@@ -9,14 +9,32 @@ import { AnnotatorService } from '../shared/annotator.service';
   styleUrls: ['./annotation-list.component.css'],
 })
 
-export class AnnotationListComponent implements OnChanges {
+export class AnnotationListComponent implements OnChanges, OnInit {
   @Input() annotations: Array<Annotation>;
+  private selectedAnnotation: Annotation = null;
 
-  constructor(
-    public annotatorService: AnnotatorService
-  ) { }
+  constructor(public annotatorService: AnnotatorService) {} 
+
+  ngOnInit(): void {
+    this.annotatorService.annotationsEvent.subscribe((event) => {
+      if (event.type === "selectAnnotation") {
+        if (event.new !== null) {
+          this.selectedAnnotation = event.new;
+        }
+      }
+    });
+  }
 
   ngOnChanges(changes: any): void {
+    console.log("update emitted");
     this.annotatorService.emitUpdate();
+  }
+
+  public annotationClick(annotation: Annotation): void {
+    this.annotatorService.selectAnnotation(annotation);
+  }
+
+  public getSelectedAnnotation(): Annotation {
+    return this.selectedAnnotation;
   }
 }

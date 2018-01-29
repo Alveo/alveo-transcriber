77e83097e5e02f331b01f0e9e2e57919095a574a
David L. Irving (SPanda) <david.l.irving@gmail.com>
2018-01-29 19:41:06 +1100

    Remove deprecated pipe

--- annotation-view/annotation.pipe.ts
+++
@@ -1,8 +0,0 @@
-import { Pipe, PipeTransform } from '@angular/core';
-
-@Pipe({ name: 'annotation' })
-export class AnnotationPipe implements PipeTransform {
-  transform(input: number): string {
-    return input.toFixed(2);
-  }
-}
--- annotator.module.ts
+++ annotator.module.ts
@@ -17,7 +17,6 @@ import { PlayerComponent } from './player/player.component';
 import { AnnotationViewComponent } from './annotation-view/annotation-view.component';
 import { AnnotationListViewComponent } from './annotation-list-view/annotation-list-view.component';
 
-import { AnnotationPipe } from './annotations/annotation.pipe';
 import { DurationPipe } from './player/duration.pipe';
 import { DurationShortPipe } from './player/duration.pipe';
 
@@ -31,7 +30,6 @@ import { Dialog } from './dialog/dialog.component';
     PlayerComponent,
     AnnotationViewComponent,
     AnnotationListViewComponent,
-    AnnotationPipe,
     DurationPipe,
     DurationShortPipe,
     Dialog,

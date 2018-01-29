6fbb232a091d4d7b74d77263c858c324d15c37a0
David L. Irving (SPanda) <david.l.irving@gmail.com>
2018-01-29 18:08:58 +1100

    Move service code into component & refactor

--- src/app/annotator/annotator.component.ts
+++ src/app/annotator/annotator.component.ts
@@ -1,11 +1,13 @@
-import { Component, ViewChild, AfterViewInit } from '@angular/core';
+import { Component, ViewChild, OnInit } from '@angular/core';
 
 import { MatDialog } from '@angular/material';
 
 import { Dialog } from './dialog/dialog.component';
 import { PlayerComponent } from './player/player.component';
 
-import { AnnotatorService } from './shared/annotator.service';
+import { AnnotatorService, ANNOTATION_CSV_FIELDS, Annotation } from './shared/annotator.service';
+
+import * as json2csv from 'json2csv';
 
 @Component({
   selector: 'annotator',
@@ -13,14 +15,23 @@ import { AnnotatorService } from './shared/annotator.service';
   styleUrls: ['./annotator.component.css'],
 })
 
-export class AnnotatorComponent {
+export class AnnotatorComponent implements OnInit {
   private viewMode = 'list';
   @ViewChild(PlayerComponent) player: PlayerComponent;
+
+  private annotations: Array<any> = [];
+  private selectedAnnotation: Annotation;
 
   constructor(
     private dialog: MatDialog,
     private annotatorService: AnnotatorService,
   ) { }
+
+  ngOnInit() {
+    this.annotations = this.annotatorService.getAnnotations();
+    this.sortAnnotations();
+    this.selectFirst();
+  }
 
   private setViewMode(mode: string) {
     this.viewMode = mode;
@@ -32,18 +43,12 @@ export class AnnotatorComponent {
 
   public setSingleView() {
     this.setViewMode('single');
-    this.annotatorService.annotationsEvent.emit({
-      type: 'resize',
-      newSize: 200,
-    })
+    this.player.resize(200);
   }
 
   public setListView() {
     this.setViewMode('list');
-    this.annotatorService.annotationsEvent.emit({
-      type: 'resize',
-      newSize: 120,
-    })
+    this.player.resize(120);
   }
 
   public actionBack(): void {
@@ -66,21 +71,25 @@ export class AnnotatorComponent {
   }
 
   exportCSV(): void {
-    const csv = this.annotatorService.dumpCSV();
+    const csv = json2csv({
+      data: this.annotations,
+      fields: ANNOTATION_CSV_FIELDS
+    });
 
     const blob = new Blob([csv], { type: 'text/csv' });
     const url = window.URL.createObjectURL(blob);
 
-    this.downloadFile(url, this.annotatorService.getAudioFileName() + '.csv');
+    this.downloadFile(url, this.getAudioFileName() + '.csv');
   }
 
   exportJSON(): void {
-    const csv = this.annotatorService.dumpJSON();
+    const csv = JSON.stringify(this.annotations, null, 2);
+    JSON.stringify(this.annotations, null, 2);
 
     const blob = new Blob([csv], { type: 'application/json' });
     const url = window.URL.createObjectURL(blob);
 
-    this.downloadFile(url, this.annotatorService.getAudioFileName() + '.json');
+    this.downloadFile(url, this.getAudioFileName() + '.json');
   }
 
   dialogOpen(title: string, text: string): any {
@@ -99,12 +108,8 @@ export class AnnotatorComponent {
     });
   }
 
-  getSelectedAnnotation(): any {
-    return this.annotatorService.getSelectedAnnotation();
-  }
-
   getAnnotations(): any {
-    return this.annotatorService.getAnnotations();
+    return this.annotations;
   }
 
   getAudioFile(): any {
@@ -152,9 +157,87 @@ export class AnnotatorComponent {
 
   public annotationEvent(ev: any): void {
     if (ev['type'] === 'select') {
-      this.annotatorService.selectAnnotation(ev['annotation']);
+      this.selectAnnotation(ev['annotation']);
     } else if (ev['type'] === 'edit') {
       this.annotatorService.emitUpdate();
+    } else if (ev['type'] === 'create') {
+      this.createAnnotationFromSegment(
+          {
+            'id': ev.id,
+            'start': ev.start,
+            'end': ev.end
+          }
+        );
     }
+  }
+
+  private sortAnnotations() {
+    this.annotations = this.annotations.sort(
+      (left, right): number => {
+        if (left.start < right.start) return -1;
+        if (left.start > right.start) return 1;
+        return 0;
+      }
+    );
+  }
+
+  public rebuild(segments: any): void {
+    this.annotations = [];
+    let counter = 0;
+
+    for (const segment of segments) {
+      this.createAnnotationFromSegment(
+          {
+            'id': counter.toString(),
+            'start': parseFloat(segment.start),
+            'end': parseFloat(segment.end),
+          }
+        );
+      counter += 1;
+    }
+
+    this.player.rebuild();
+
+    this.selectFirst();
+  }
+
+  public selectFirst() {
+    if (this.annotations.length > 0) {
+      this.selectAnnotation(this.annotations[0]);
+    } else {
+      this.selectAnnotation(null);
+    }
+  }
+
+  public createAnnotationFromSegment(segment: any): Annotation {
+    const annotation = new Annotation(
+      segment['id'],
+      segment['start'],
+      segment['end'],
+      segment['speaker'],
+      segment['caption'],
+      segment['cap_type']
+    );
+
+    this.annotations.push(annotation);
+    this.sortAnnotations();
+
+    return annotation;
+  }
+
+  public selectAnnotation(annotation: Annotation) {
+    const oldSelection = this.selectedAnnotation;
+    this.selectedAnnotation = annotation;
+
+    this.player.selectAnnotation(
+      {
+        'new': annotation,
+        'old': oldSelection
+      }
+    );
+  }
+
+  public getSelectedAnnotation(): Annotation {
+    return this.selectedAnnotation;
   }
 }
--- src/app/annotator/shared/annotator.service.ts
+++ src/app/annotator/shared/annotator.service.ts
@@ -1,8 +1,7 @@
 import { Injectable, EventEmitter } from '@angular/core';
 
-import * as json2csv from 'json2csv';
 
-const ANNOTATION_CSV_FIELDS = [
+export const ANNOTATION_CSV_FIELDS = [
   'id',
   'start',
   'end',
@@ -40,7 +39,6 @@ export class Annotation {
 
 @Injectable()
 export class AnnotatorService {
-  public annotationsEvent: EventEmitter<any> = new EventEmitter();
   public annotationsUpdate: EventEmitter<any> = new EventEmitter();
   public externalEvent: EventEmitter<any> = new EventEmitter();
 
@@ -49,26 +47,12 @@ export class AnnotatorService {
 
   public annotations: Array<Annotation> = [];
 
-  private selectedAnnotation: Annotation;
-
-  public getSelectedAnnotation(): Annotation {
-    return this.selectedAnnotation;
-  }
-
   public rebase(annotations: Array<Annotation>): any {
     this.annotations = annotations;
-    this.sortAnnotations();
-    this.selectFirst();
   }
 
-  private sortAnnotations() {
-    this.annotations = this.annotations.sort(
-      (left, right): number => {
-        if (left.start < right.start) return -1;
-        if (left.start > right.start) return 1;
-        return 0;
-      }
-    );
+  public rebuild(thing: any): any {
+    console.log(thing);
   }
 
   public emitUpdate(): any {
@@ -77,103 +61,6 @@ export class AnnotatorService {
 
   public getAnnotations(): any {
     return this.annotations;
-  }
-
-  public rebuild(segments: any): void {
-    this.annotations = [];
-    let counter = 0;
-
-    for (const segment of segments) {
-      this.createAnnotationFromSegment(
-          {
-            'id': counter.toString(),
-            'start': parseFloat(segment.start),
-            'end': parseFloat(segment.end),
-          }
-        );
-      counter += 1;
-    }
-
-    this.annotationsEvent.emit({'type': 'rebuild'});
-
-    this.selectFirst();
-  }
-
-  public selectFirst() {
-    if (this.annotations.length > 0) {
-      this.selectAnnotation(this.annotations[0]);
-    } else {
-      this.selectAnnotation(null);
-    }
-  }
-
-  public createAnnotationFromSegment(segment: any): Annotation {
-    const annotation = new Annotation(
-      segment['id'],
-      segment['start'],
-      segment['end'],
-      segment['speaker'],
-      segment['caption'],
-      segment['cap_type']
-    );
-
-    this.annotations.push(annotation);
-    this.sortAnnotations();
-
-    this.selectAnnotation(annotation);
-
-    return annotation;
-  }
-
-  public deleteAnnotationByID(id: string): boolean {
-    for (const annotation of this.annotations) {
-      if (annotation.id === id) {
-        if (id = this.selectedAnnotation.id) {
-          this.selectedAnnotation = null;
-        }
-
-        const index = this.annotations.indexOf(annotation);
-        if (index !== -1) {
-          this.annotations.splice(index, 1);
-        }
-
-        return true;
-      }
-    }
-    return false;
-  }
-
-  public getAnnotationByID(id: string): Annotation {
-    for (const annotation of this.annotations) {
-      if (annotation.id === id) {
-        return annotation;
-      }
-    }
-    return null;
-  }
-
-  public selectAnnotation(annotation: Annotation) {
-    const oldSelection = this.selectedAnnotation;
-    this.selectedAnnotation = annotation;
-
-    this.annotationsEvent.emit(
-      {
-        'type': 'selectAnnotation',
-        'new': annotation,
-        'old': oldSelection
-      }
-    );
-  }
-
-  public dumpCSV(): string {
-    return json2csv({
-      data: this.annotations,
-      fields: ANNOTATION_CSV_FIELDS
-    });
-  }
-
-  public dumpJSON(): any {
-    return JSON.stringify(this.annotations, null, 2);
   }
 
   public getAudioFile(): any {

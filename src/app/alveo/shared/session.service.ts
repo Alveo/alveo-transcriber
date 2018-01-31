86405663ecbece40bdf892de0c8114ed5a25d3fe
David L. Irving (SPanda) <david.l.irving@gmail.com>
2018-01-31 21:45:06 +1100

    Simplify, fixes

--- src/app/alveo/shared/session.service.ts
+++ src/app/alveo/shared/session.service.ts
@@ -1,67 +1,42 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import { Router } from '@angular/router';
-import 'rxjs/Observable';
 
 import { DBService, Databases } from './db.service';
-import { AlveoService } from './alveo.service';
 
 import { Paths } from './paths';
 
 @Injectable()
 export class SessionService {
   private stored_route: any = [''];
-  private loading: any = [];
+  private ready: boolean = false;
 
   constructor(
     private router: Router,
-    private alveoService: AlveoService,
     private dbService: DBService) {
 
-    this.annotationEventSubscribe();
-    this.loading.push('dbservice');
     this.dbService.instance(Databases.Cache).get('sessionService').then(
       data => {
         console.log('Stored session data has been found and loaded.');
 
         this.stored_route = data['stored_route'];
-
-        this.loading.pop(this.loading.filter(inst => inst === 'dbservice'));
+        this.ready = true;
       },
       error => {
         console.log('Stored session data not found. Initialising.');
 
-        this.updateStorage();
-
-        this.loading.pop(this.loading.filter(inst => inst === 'dbservice'));
+        this.updateStorage().then(
+          () => this.ready = true
+        );
       }
     );
-  }
-
-  private annotationEventSubscribe() {
-    /* TODO
-    this.annotationsService.serviceEvent.subscribe(
-      (event) => {
-        if (event === 'exit') {
-          this.navigate([Paths.ListView]);
-        }
-      }
-    );
-     */
-  }
-
-  public isLoading(): boolean {
-    if (this.loading.length > 0) {
-      return true;
-    }
-    return false;
   }
 
   public onReady(): Promise<any> {
     return new Promise(
       (resolve, reject) => {
         const interval = setInterval(() => {
-          if (this.loading.length === 0) {
+          if (this.ready) {
             clearInterval(interval);
             resolve();
           }
@@ -103,16 +78,19 @@ export class SessionService {
   /* Navigate to the specified route if possible */
   public navigate(route: any[]): Promise<any> {
     return new Promise(
-      (success, error) => {
-        this.router.navigate(route)
-          .then(data => {
+      (resolve, reject) => {
+        this.router.navigate(route).then(
+          (data) => {
             this.stored_route = route;
-            this.updateStorage();
-            success(data);
+            this.updateStorage().then(
+              () => resolve(data)
+            );
           })
-          .catch(errorMsg =>  {
-            error(errorMsg)
-          })
+        .catch(
+          (error) => {
+            reject(error)
+          }
+        );
       }
     );
   }

c1d4b1340b8ec6b79c7fc0f59fc8762cf4951bc5
David L. Irving (SPanda) <david.l.irving@gmail.com>
2018-01-31 22:17:17 +1100

    Display annotation count

--- src/app/alveo/lists/items/docs/docs.component.ts
+++ src/app/alveo/lists/items/docs/docs.component.ts
@@ -1,4 +1,7 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter, OnInit } from '@angular/core';
+
+import { Annotation } from '../../../../annotator/shared/annotation';
+import { AnnotationService } from '../../../shared/annotation.service';
 
 /* Display component for showing and selecting of docs
  *   Provides route for Annotator module */
@@ -7,14 +10,31 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
   templateUrl: './docs.component.html',
   styleUrls: ['./docs.component.css'],
 })
-export class DocsComponent {
-  @Input() alveo_doc: any;
+export class DocsComponent implements OnInit {
+  @Input() alveo_doc: any = null;
   @Output() onNavigate = new EventEmitter<any>();
+  private annotations: Array<Annotation> = [];
 
-  constructor() { }
+  constructor(
+    private annotationService: AnnotationService
+  ) { }
+
+  ngOnInit() {
+    if (this.alveo_doc !== null) {
+      this.annotationService.loadAnnotations(this.getDocName()).then(
+        (annotations) => {
+          this.annotations = annotations;
+        }
+      );
+    }
+  }
 
   public getDocName(): string {
     return this.alveo_doc['dcterms:identifier'];
+  }
+
+  public getAnnotationCount(): number {
+    return this.annotations.length;
   }
 
   public navigate(): void {

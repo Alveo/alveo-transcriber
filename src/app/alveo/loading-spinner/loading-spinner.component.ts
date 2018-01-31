efaa06033694f220445bbeb5f18dd6496bbf9fee
David L. Irving (SPanda) <david.l.irving@gmail.com>
2018-01-31 20:59:25 +1100

    Add sizing options

--- src/app/alveo/loading-spinner/loading-spinner.component.ts
+++ src/app/alveo/loading-spinner/loading-spinner.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'loading-spinner',
@@ -6,5 +6,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./loading-spinner.component.css']
 })
 export class LoadingSpinnerComponent {
+  @Input() spinnerSize: string = "large";
+
   constructor() { }
+
+  public usePageFill(): boolean {
+    console.log(this.spinnerSize);
+    if (this.spinnerSize === "large") {
+      return true;
+    }
+    return false;
+  }
+
+  public getSpinnerDiameter(): number {
+    switch (this.spinnerSize) {
+      case "small": {
+        return 40;
+      }
+      case "medium": {
+        return 80;
+      }
+      case "large": {
+        return 140;
+      }
+    }
+  }
 }

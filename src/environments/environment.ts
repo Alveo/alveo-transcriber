72e88a059cf623c73a44a8ab226e6182539c39d2
David L. Irving (SPanda) <david.l.irving@gmail.com>
2018-01-31 21:35:55 +1100

    Add new URL structure

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,14 @@
 export const environment = {
   production: false,
 
-  baseURL: 'https://app.alveo.edu.au',
-  loginURL: '/oauth/authorize',
+  alveoPaths: {
+    mainUrl: 'https://app.alveo.edu.au',
+    listSuffix: "item_lists",
+    itemSuffix: "catalog/austalk",
+    apiKeySuffix: 'account_api_key',
+    oAuthTokenSuffix: 'oauth/token',
+    loginSuffix: 'oauth/authorize'
+  },
 
   clientID: 'e37df76b698449298761b34c14654ba871e694f81be42cac483c1dc8b529279e',
   clientSecret: '2b83c1b2382d0f76b62729af99a4dba64aac61dcf7827a4d5df60e98c1975055',

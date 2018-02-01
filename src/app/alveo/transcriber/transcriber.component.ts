e1cbe2532bcccbdcd450444ecc3ebc291c1f2f3d
David L. Irving (SPanda) <david.l.irving@gmail.com>
2018-02-01 16:14:05 +1100

    Finish integration into Alveo module

--- src/app/alveo/transcriber/transcriber.component.ts
+++ src/app/alveo/transcriber/transcriber.component.ts
@@ -1,9 +1,15 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+
+import { AlveoService } from '../shared/alveo.service';
+import { AnnotationService } from '../shared/annotation.service';
+import { AuthService } from '../shared/auth.service';
+import { SessionService } from '../shared/session.service';
+import { SegmentorService } from '../shared/segmentor.service';
 
 import { AnnotatorComponent } from '../../annotator/annotator.component'
 import { Annotation } from '../../annotator/shared/annotation';
-import { SegmentorService } from '../shared/segmentor.service';
-import { DBService, Databases } from '../shared/db.service';
+import { Paths } from '../shared/paths';
 
 @Component({
   selector: 'transcriber',
@@ -11,68 +17,156 @@ import { DBService, Databases } from '../shared/db.service';
   styleUrls: ['./transcriber.component.css']
 })
 export class TranscriberComponent implements OnInit {
-  // doc as route input
   private ready: boolean = false;
+
+  private item: any = null;
+  private audioFileData: ArrayBuffer = null;
+  private annotations: Array<Annotation> = [];
+  private selectedAnnotation: Annotation = null;
+  private defaultView: string = "list";
 
   @ViewChild(AnnotatorComponent) annotator: AnnotatorComponent;
 
   constructor(
     private segmentorService: SegmentorService,
-    private dbService: DBService) {
+    private annotationService: AnnotationService,
+    private alveoService: AlveoService,
+    private sessionService: SessionService,
+    private authService: AuthService,
+    private activatedRoute: ActivatedRoute,
+  ) { }
+
+  ngOnInit(): void {
+    this.activatedRoute.params.subscribe(
+      (params: Params) => {
+        const item_id = params['id'];
+        if (item_id === undefined) {
+          this.sessionService.navigate([Paths.ListIndex]);
+        } else {
+          this.prepareItem(item_id).then(
+            (item) => {
+              this.item = item;
+              this.prepareAudioFile(item_id, item['alveo:documents'][0]['dcterms:identifier']).then(
+                (data) => {
+                  this.audioFileData = data;
+
+                  this.loadAnnotations(this.getIdentifier()).then(
+                    (annotations) => {
+                      this.annotations = annotations;
+                      this.ready = true;
+                    }
+                  ).catch(
+                    () => {
+                      this.annotations = [];
+                      this.ready = true;
+                    }
+                  );
+                }
+              ).catch((error) => console.log(error));
+            }
+          ).catch((error) => console.log(error));
+        }
+      }
+    );
   }
 
-  ngOnInit() {
-    // route input for doc
+  public isReady(): boolean {
+    return this.ready;
   }
 
-  getAudioFile() {
-    //return this.
-  }
-
-  public getAnnotations(identifier: string): Promise<any> {
+  private prepareItem(list_id: string): Promise<any> {
     return new Promise(
-      (resolve, error) => {
-        this.dbService.instance(Databases.Annotations).get(identifier)
-          .then((data) => resolve(data['annotations']))
-          .catch((error) => resolve([]))
+      (resolve, reject) => {
+        this.alveoService.getItem(list_id).subscribe(
+          list => {
+            resolve(list);
+          },
+          error => {
+            if (error === 403 && !this.authService.isLoggedIn()) {
+              this.authService.promptLogin();
+            }
+            reject(error);
+          }
+        );
       }
     );
   }
 
-  private autoSegment(fileUrl: string) {
-    if (fileUrl === '') {
-      //raise exc
+  public getAudioFileUrl(): string {
+    if (this.item === null) {
+      return "";
     }
+    return this.item['alveo:documents'][0]['alveo:url'];
+  }
 
-    this.segmentorService.segment(fileUrl,
+  public getIdentifier(): string {
+    if (this.item === null) {
+      return null;
+    }
+    return this.item['alveo:metadata']['dc:title'];
+  }
+  
+  private prepareAudioFile(list_id: string, doc_id: string): Promise<any> {
+    return new Promise(
+      (resolve, reject) => {
+        this.alveoService.getAudioFile(list_id, doc_id).subscribe(
+          audioData => {
+            resolve(audioData);
+          },
+          error => {
+            if (error === 403 && !this.authService.isLoggedIn()) {
+              this.authService.promptLogin();
+            }
+            reject(error);
+          }
+        );
+      }
+    );
+  }
+
+
+  public getAudioFile() {
+    return this.audioFileData;
+  }
+
+  public getAnnotations() {
+    return this.annotations;
+  }
+
+  public getSelectedAnnotation() {
+    return this.selectedAnnotation;
+  }
+
+  public getViewMode() {
+    return this.defaultView;
+  }
+
+  public exit(ev: any) {
+    if (this.item === null) {
+      this.sessionService.navigate([Paths.ListIndex]);
+    } else {
+      this.sessionService.navigate([Paths.ListView+'/'+this.getIdentifier()]);
+    }
+  }
+
+  public loadAnnotations(identifier: string): Promise<any> {
+    return this.annotationService.loadAnnotations(identifier);
+  }
+
+  public saveAnnotations(ev: any): Promise<any> {
+    return this.annotationService.saveAnnotations(this.getIdentifier(), ev['annotations']);
+  }
+
+  private autoSegment(ev: any): void {
+    this.segmentorService.segment(this.getAudioFileUrl(),
       (data) => {
-        this.annotatorService.rebuild(data.json());
-        this.setAnnotations(this.docIdentifier, data.json());
+        this.annotator.rebuild(data).then(
+          (annotations) => {
+            this.annotations = annotations;
+            this.annotationService.saveAnnotations(this.getIdentifier(), annotations);
+          }
+        ).catch((error) => console.log(error));
       }
     );
-  }
-
-  public setAnnotations(docIdentifier: string, annotations: Array<Annotation>): Promise<any> {
-    return this.dbService.instance(Databases.Annotations)
-      .put(docIdentifier, {'annotations': annotations});
-  }
-
-  public watch(docIdentifier: string, annotatorWatcher: any) {
-    if (this.annotatorEvent != null) {
-      this.annotatorEvent.unsubscribe();
-      this.annotatorEvent = null;
-    }
-
-    this.docIdentifier = docIdentifier;
-
-    this.annotatorEvent = annotatorWatcher.subscribe((ev: any) => {
-      if (ev['type'] === 'exit') {
-        this.serviceEvent.emit('exit');
-      } else if (ev['type'] === 'autosegment') {
-        this.autoSegment(this.fileUrl);
-      } else if (ev['type'] === 'save') {
-        this.setAnnotations(docIdentifier, ev['annotations']);
-      }
-    });
   }
 }

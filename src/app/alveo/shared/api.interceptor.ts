c825c5e388b8963c3dec81e085fa0482bd372d13
David L. Irving (SPanda) <david.l.irving@gmail.com>
2018-02-01 16:12:35 +1100

    Only send API key to alveoUrls

--- src/app/alveo/shared/api.interceptor.ts
+++ src/app/alveo/shared/api.interceptor.ts
@@ -10,6 +10,7 @@ import {
 } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
 import { AuthService } from './auth.service';
+import { environment } from '../../../environments/environment';
 
 @Injectable()
 export class ApiInterceptor implements HttpInterceptor {
@@ -18,14 +19,16 @@ export class ApiInterceptor implements HttpInterceptor {
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     const authService = this.injector.get(AuthService);
 
-    request = request.clone({
-      setHeaders: {
-        'X-Api-Key': authService.getApiKey(),
-        'Accept': 'application/json'
-      }
-    });
+    if (request.url.startsWith(environment.alveoPaths.mainUrl)) {
+      request = request.clone({
+        setHeaders: {
+          'X-Api-Key': authService.getApiKey(),
+          'Accept': 'application/json'
+        }
+      });
 
-    console.log('Made API request to ' + request.url);
+      console.log('Made API request to ' + request.url);
+    }
 
     return next.handle(request);
   }

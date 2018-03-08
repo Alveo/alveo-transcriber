bb0f8eaaf097614c29e6bf8f7e09c2250f856a0b
David L. Irving (SPanda) <david.l.irving@gmail.com>
2018-03-08 13:17:42 +1100

    Update API callback url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -13,7 +13,7 @@ export const environment = {
   clientID: 'e37df76b698449298761b34c14654ba871e694f81be42cac483c1dc8b529279e',
   clientSecret: '2b83c1b2382d0f76b62729af99a4dba64aac61dcf7827a4d5df60e98c1975055',
 
-  callbackURL: window.location.origin + '/oauth/callback',
+  callbackURL: window.location.origin + '/alveo-transcriber' + '/oauth/callback',
 
   devTools: true,
 

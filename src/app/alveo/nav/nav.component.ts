fa6b96703dd15f7553cc27f491a905dbaf25be60
David L. Irving (SPanda) <david.l.irving@gmail.com>
2018-04-17 11:45:37 +1000

    Add item list and alveo app server url

--- src/app/alveo/nav/nav.component.ts
+++ src/app/alveo/nav/nav.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../shared/auth.service';
+import { SessionService } from '../shared/session.service';
+import { Paths } from '../shared/paths';
+
+import { environment } from '../../../environments/environment';
 
 /* Navigation bar component, primarily handles logout option */
 @Component({
@@ -8,7 +12,10 @@ import { AuthService } from '../shared/auth.service';
   styleUrls: ['./nav.component.css'],
 })
 export class NavComponent {
-  constructor(private authService: AuthService) { }
+  constructor(
+    private authService: AuthService,
+    private sessionService: SessionService
+  ) { }
 
   public isLoggedIn(): boolean {
     return this.authService.isLoggedIn();
@@ -20,5 +27,13 @@ export class NavComponent {
 
   public actionLogout(): void {
     this.authService.logout();
+  }
+
+  public goIndex(): void {
+    this.sessionService.navigate([Paths.ListIndex]);
+  }
+
+  public goAlveoVL(): void {
+    location.href = environment.alveoPaths.mainUrl;
   }
 }

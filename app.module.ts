fc64ec90acbecd2cd10739a8ba1f01c5113596d7
David L. Irving (SPanda) <david.l.irving@gmail.com>
2018-04-17 13:31:44 +1000

    Initial commit

---
+++ app.module.ts
@@ -0,0 +1,23 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+
+import { AnnotatorModule } from './transcriber/annotator.module';
+
+import { AppComponent } from './app.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    CommonModule,
+    AnnotatorModule,
+  ],
+  exports: [
+    AnnotatorModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }

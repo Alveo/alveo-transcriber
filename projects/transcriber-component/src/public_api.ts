e27cf2bb276249c90122a58df709d88afb7ef914
David L. Irving (SPanda) <david.l.irving@gmail.com>
2018-05-09 12:00:32 +1000

    Restructure for Angular6 library conversion

--- projects/transcriber-component/src/public_api.ts
+++ projects/transcriber-component/src/public_api.ts
@@ -1,7 +1,1 @@
-/*
- * Public API Surface of transcriber-component
- */
-
-export * from './lib/transcriber-component.service';
-export * from './lib/transcriber-component.component';
-export * from './lib/transcriber-component.module';
+export * from './lib/alveo-transcriber.module';

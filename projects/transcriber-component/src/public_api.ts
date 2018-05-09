5509c73ca6d6b91e2ed58daf4f94fd6f5b078146
David L. Irving (SPanda) <david.l.irving@gmail.com>
2018-05-09 11:35:32 +1000

    Init Angular6 library for the transcriber component

---
+++ projects/transcriber-component/src/public_api.ts
@@ -0,0 +1,7 @@
+/*
+ * Public API Surface of transcriber-component
+ */
+
+export * from './lib/transcriber-component.service';
+export * from './lib/transcriber-component.component';
+export * from './lib/transcriber-component.module';

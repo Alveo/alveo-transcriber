4b73446a399936d3f207ae603bdf9119949b90f4
David L. Irving (SPanda) <david.l.irving@gmail.com>
2018-06-17 16:02:06 +1000

    Update urls

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -7,8 +7,15 @@ export const environment = {
     itemSuffix: 'catalog',
     apiKeySuffix: 'account_api_key',
     oAuthTokenSuffix: 'oauth/token',
-    loginSuffix: 'oauth/authorize'
+    loginSuffix: 'oauth/authorize',
+    userDetails: '/account/get_details'
   },
+
+  alveoTransServPaths: {
+    segmenterUrl: 'https://segmenter.apps.alveo.edu.au/alveo',
+    segmenterSuffix: '/segment'
+  },
+  alveoTransServAuth: 'app.alveo.edu.au',
 
   clientID: 'e37df76b698449298761b34c14654ba871e694f81be42cac483c1dc8b529279e',
   clientSecret: '2b83c1b2382d0f76b62729af99a4dba64aac61dcf7827a4d5df60e98c1975055',
@@ -16,7 +23,4 @@ export const environment = {
   callbackURL: window.location.origin + '/alveo-transcriber' + '/oauth/callback',
 
   devTools: true,
-
-  segmenterUrl: 'https://segmenter.apps.alveo.edu.au/alveo',
-  segmenterAuth: 'app.alveo.edu.au'
 };
--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -2,13 +2,20 @@ export const environment = {
   production: true,
 
   alveoPaths: {
-    mainUrl: 'https://app.alveo.edu.au',
+    mainUrl: 'https://staging.alveo.edu.au',
     listSuffix: 'item_lists',
     itemSuffix: 'catalog',
     apiKeySuffix: 'account_api_key',
     oAuthTokenSuffix: 'oauth/token',
-    loginSuffix: 'oauth/authorize'
+    loginSuffix: 'oauth/authorize',
+    userDetails: '/account/get_details'
   },
+
+  alveoTransServPaths: {
+    segmenterUrl: 'https://segmenter.apps.alveo.edu.au/alveo',
+    segmenterSuffix: '/segment'
+  },
+  alveoTransServAuth: 'app.alveo.edu.au',
 
   clientID: 'e37df76b698449298761b34c14654ba871e694f81be42cac483c1dc8b529279e',
   clientSecret: '2b83c1b2382d0f76b62729af99a4dba64aac61dcf7827a4d5df60e98c1975055',
@@ -16,7 +23,4 @@ export const environment = {
   callbackURL: window.location.origin + '/oauth/callback',
 
   devTools: true,
-
-  segmenterUrl: 'https://segmenter.apps.alveo.edu.au/alveo',
-  segmenterAuth: 'app.alveo.edu.au'
 };

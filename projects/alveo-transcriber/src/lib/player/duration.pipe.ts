6bfc5e8291eec7f0cbcc52128575ab5ac1c08ead
David L. Irving (SPanda) <david.l.irving@gmail.com>
2018-07-24 08:52:16 +1000

    Implement result caching

--- projects/alveo-transcriber/src/lib/player/duration.pipe.ts
+++ projects/alveo-transcriber/src/lib/player/duration.pipe.ts
@@ -2,26 +2,44 @@ import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({ name: 'duration' })
 export class DurationPipe implements PipeTransform {
+  private previousValue: number;
+  private cachedResult: string;
+
   transform(input: number): string {
+    if(input == this.previousValue && this.cachedResult) {
+      return this.cachedResult;
+    }
+    this.previousValue = input;
+
     const seconds = Math.floor((input % 60000) / 1000);
     const minutes = Math.floor(input / 60000);
 
     let formatted = (minutes > 0 ? minutes + ' minutes' : '');
     formatted += (seconds > 0 ? ' ' + seconds + ' seconds' : '');
 
-    return formatted;
+    this.cachedResult = formatted;
+    return this.cachedResult;
   }
 }
 
 @Pipe({ name: 'durationshort' })
 export class DurationShortPipe implements PipeTransform {
+  private previousValue: number;
+  private cachedResult: string;
+
   transform(input: number): string {
+    if(input == this.previousValue && this.cachedResult) {
+      return this.cachedResult;
+    }
+    this.previousValue = input;
+
     const seconds = Math.floor((input % 60));
     const minutes = Math.floor(input / 60);
 
     let formatted = (minutes > 0 ? minutes + ':' : '0:');
     formatted += (seconds > 0 ? '' + (seconds > 9 ? seconds : '0' + seconds) : '00');
 
-    return formatted;
+    this.cachedResult = formatted;
+    return this.cachedResult;
   }
 }

acd4625aba37f15a4027c2d2dbf35113c24972bb
David L. Irving (SPanda) <david.l.irving@gmail.com>
2018-08-01 12:36:02 +1000

    Handle async

--- projects/alveo-transcriber/src/lib/shared/annotation-exporter.service.spec.ts
+++ projects/alveo-transcriber/src/lib/shared/annotation-exporter.service.spec.ts
@@ -1,4 +1,5 @@
 import { TestBed, inject } from '@angular/core/testing';
+import { async } from '@angular/core/testing';
 
 import { Annotation } from './annotation';
 import { AnnotationExporterService } from './annotation-exporter.service';
@@ -8,7 +9,7 @@ import * as _ from 'lodash';
 describe('AnnotationExporterService', () => {
   function generateAnnotations(): Array<Annotation> {
     let annotations = new Array<Annotation>();
-    for (let i=0; i<5; i++) {
+    for (let i=0; i<14; i++) {
       annotations.push(new Annotation(i.toString(), 0, 0, 'Speaker', 'caption', "unittest"))
     }
     return annotations;
@@ -24,7 +25,7 @@ describe('AnnotationExporterService', () => {
     expect(service).toBeTruthy();
   }));
 
-  it('should create a valid csv download', inject([AnnotationExporterService], (service: AnnotationExporterService) => {
+  it('should create a valid csv download', async(inject([AnnotationExporterService], (service: AnnotationExporterService) => {
     const annotations = generateAnnotations();
 
     let urlCreateObjectSpy = spyOn(URL, 'createObjectURL').and.callFake(
@@ -50,12 +51,12 @@ describe('AnnotationExporterService', () => {
       }
     );
 
-    // TODO filename?
+    // TODO test filename?
     
-    let a = service.asCSV("test.csv", annotations);
-  }));
+    let a = service.asCSVAttachment("test.csv", annotations);
+  })));
 
-  it('should create a valid json download', inject([AnnotationExporterService], (service: AnnotationExporterService) => {
+  it('should create a valid json download', async(inject([AnnotationExporterService], (service: AnnotationExporterService) => {
     const annotations = generateAnnotations();
 
     let urlCreateObjectSpy = spyOn(URL, 'createObjectURL').and.callFake(
@@ -64,8 +65,7 @@ describe('AnnotationExporterService', () => {
 
         let reader = new FileReader();
         reader.onload = () => {
-          const json = JSON.parse(reader.result);
-          const expAnnotations = json.annotations;
+          const expAnnotations = JSON.parse(reader.result);
 
           // This is done because lodash will fail the isEqual check
           //  as `annotations` is Array<Annotations> but expAnnotations
@@ -78,9 +78,8 @@ describe('AnnotationExporterService', () => {
       }
     );
 
-    // TODO filename?
+    // TODO test filename?
     
-    let a = service.asJSON("test.json", annotations);
-  }));
-
+    let a = service.asJSONAttachment("test.json", annotations);
+  })));
 });

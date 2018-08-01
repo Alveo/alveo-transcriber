3cc5f55cdc15d7e0d043161b7e4b68e76bd90644
David L. Irving (SPanda) <david.l.irving@gmail.com>
2018-08-01 11:37:43 +1000

    Create CSV export unit test

--- projects/alveo-transcriber/src/lib/shared/annotation-exporter.service.spec.ts
+++ projects/alveo-transcriber/src/lib/shared/annotation-exporter.service.spec.ts
@@ -1,8 +1,18 @@
 import { TestBed, inject } from '@angular/core/testing';
 
+import { Annotation } from './annotation';
 import { AnnotationExporterService } from './annotation-exporter.service';
 
+
 describe('AnnotationExporterService', () => {
+  function generateAnnotations(): Array<Annotation> {
+    let annotations = new Array<Annotation>();
+    for (let i=0; i<5; i++) {
+      annotations.push(new Annotation(i.toString(), 0, 0, 'Speaker', 'caption', "unittest"))
+    }
+    return annotations;
+  }
+
   beforeEach(() => {
     TestBed.configureTestingModule({
       providers: [AnnotationExporterService]
@@ -12,4 +22,37 @@ describe('AnnotationExporterService', () => {
   it('should be created', inject([AnnotationExporterService], (service: AnnotationExporterService) => {
     expect(service).toBeTruthy();
   }));
+
+  it('should create a valid csv download', inject([AnnotationExporterService], (service: AnnotationExporterService) => {
+    const annotations = generateAnnotations();
+
+    let urlCreateObjectSpy = spyOn(URL, 'createObjectURL').and.callFake(
+      (blob) => {
+        expect(blob.type).toBe("text/csv");
+
+        let reader = new FileReader();
+        reader.onload = () => {
+          const csv = reader.result.split("\n");
+          expect(csv[0]).toBe('"id","start","end","speaker","caption","cap_type"');
+
+          for (const annotation in annotations) {
+            expect(csv[+annotation+1]).toBe("\"" + annotations[annotation].id + "\","
+              + annotations[annotation].start.toString() + ","
+              + annotations[annotation].end.toString() + ","
+              + "\"" + annotations[annotation].speaker + "\","
+              + "\"" + annotations[annotation].caption + "\","
+              + "\"" + annotations[annotation].cap_type + "\""
+            );
+          }
+        }
+        reader.readAsText(blob);
+      }
+    );
+
+    // TODO filename?
+    
+    let a = service.asCSV("test.csv", annotations);
+  }));
+
+  //JSON.parse
 });

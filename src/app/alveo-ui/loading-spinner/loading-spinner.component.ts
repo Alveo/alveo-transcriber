26fdd2ccc5d8b62ddbdbd86aead3cdf1e4ce559c
David L. Irving (SPanda) <david.l.irving@gmail.com>
2018-08-01 16:20:34 +1000

    Refactor, standardize, comment

--- src/app/alveo-ui/loading-spinner/loading-spinner.component.ts
+++ src/app/alveo-ui/loading-spinner/loading-spinner.component.ts
@@ -1,5 +1,28 @@
 import { Component, Input } from '@angular/core';
 
+/* The LoadingSpinnerComponent provides us a lazy but consistent
+ * way to have a mat-spinner active for the incredible amounts
+ * of Promises and Observables the application is built upon.
+ *
+ * It takes the following inputs:
+ * @ spinnerSize: string: Supported values are:
+ *    'tiny', 'small', 'medium', 'large'
+ *
+ *   Labels are used to keep sizing consistent across components,
+ *   but perhaps this is not completely needed and should be
+ *   explored further.
+ *
+ *   If set to 'large', the component will utilise most of the view
+ *   space, generally useful if no further action can be done until
+ *   a promise completes.
+ *
+ *   Default is 'large'.
+ *
+ * @ inline: boolean
+ *   If set to true, the component will avoid making line breaks.
+ *
+ *   Default is false.
+ */
 @Component({
   selector: 'loading-spinner',
   templateUrl: './loading-spinner.component.html',
